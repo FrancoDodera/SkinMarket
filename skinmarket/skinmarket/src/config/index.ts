e21500ff27be9d23e0573025a73b735c7284920d
FrancoDodera <francododera@hotmail.com>
2023-12-15 21:36:34 -0300

    Empezando
    Mi primer push de la app

---
+++ skinmarket/skinmarket/src/config/index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "Skins",
+    value: "ui_kits" as const,
+    featured: [
+      {
+        name: "Guns",
+        href: "#",
+        imageSrc: "/nav/Skins/rifles.png",
+      },
+      {
+        name: "Knife & Gloves",
+        href: "#",
+        imageSrc: "/nav/Skins/knife&gloves.png",
+      },
+      {
+        name: "Stickers & Grafity",
+        href: "#",
+        imageSrc: "/nav/Skins/stickers&grafity.png",
+      },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "Editor picks",
+        href: "#",
+        // imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+      {
+        name: "Editor picks",
+        href: "#",
+        // imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+      {
+        name: "Editor picks",
+        href: "#",
+        // imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+    ],
+  },
+];
