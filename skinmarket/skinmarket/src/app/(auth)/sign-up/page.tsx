0eb774cf51e1ff942e0c8dfa9c9aabef7bd5f9ba
FrancoDodera <francododera@hotmail.com>
2024-01-04 18:33:26 -0300

    trpc y server
    trpc y server

---
+++ skinmarket/skinmarket/src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import logo from "../../../assets/logo-removebg-preview.png";
+import { ArrowRight } from "lucide-react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  TAuthCredentialsValidator,
+  AuthCredentialsValidator,
+} from "@/lib/validators/account-credentials-validator";
+import { trpc } from "@/trpc/client";
+
+const Page = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TAuthCredentialsValidator>({
+    resolver: zodResolver(AuthCredentialsValidator),
+  });
+  const { data } = trpc.anyApiRoute.useQuery();
+  console.log(data);
+
+  const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {};
+
+  return (
+    <>
+      <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col items-center space-y-2 text-center">
+            <img className="h-20 w-20" src={logo.src} alt="" />
+            <h1 className="text-2x1 font-bold">Create an account</h1>
+            <Link
+              className={buttonVariants({
+                variant: "link",
+                className: "gap-1.5",
+              })}
+              href="/sign-in"
+            >
+              Aldery have an account? Sign-in
+              <ArrowRight className="h-4 2-4" />
+            </Link>
+          </div>
+          <div className="grid gap-6">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="grid gap-2">
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    {...register("email")}
+                    className={cn({
+                      "focus-visible:ring-red-500": errors.email,
+                    })}
+                    placeholder="exaple@gmail.com"
+                  />
+                </div>
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    {...register("password")}
+                    className={cn({
+                      "focus-visible:ring-red-500": errors.password,
+                    })}
+                    placeholder="Password"
+                  />
+                </div>
+                <Button>Sign up</Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page;
